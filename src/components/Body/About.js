05b2ae68a883938f438764f620554005d716c0e4
nepomn9shih <nepomn9shih@gmail.com>
2021-01-10 18:02:31 +0300

    Добавил Swiper в About

--- src/components/Body/About.js
+++ src/components/Body/About.js
@@ -1,7 +1,36 @@
+import { Swiper, SwiperSlide } from "../../../node_modules/swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+} from "../../../node_modules/swiper";
+
+SwiperCore.use([Navigation, Pagination]);
+
 const About = () => {
+  const slides = [];
+  for (let i = 0; i < 5; i++) {
+    slides.push(
+      <SwiperSlide key={`slide-${i}`} tag="li">
+        <img
+          src={`https://picsum.photos/id/${i + 1}/500/300`}
+          alt={`slide-${i}`}
+        />
+      </SwiperSlide>
+    );
+  }
+
   return (
     <section class="content__about about">
       <h1 class="content__title">О сайте</h1>
+      <Swiper
+        id="main"
+        tag="section"
+        wrapperTag="ul"
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {slides}
+      </Swiper>
       <div class="about__text">
         <p>
           "Данный сайт является резюме-визиткой, содержащий информацию обо мне,
@@ -9,8 +38,10 @@ const About = () => {
           ссылки: на мой репозиторий на GitHub, на мое резюме на HH.ru, на мою
           страницу в LinkedIn."
         </p>
-        <br /> 
-        <p><b>Непомнящих Александр, начинающий React frontend разработчик.</b></p>
+        <br />
+        <p>
+          <b>Непомнящих Александр, начинающий React frontend разработчик.</b>
+        </p>
       </div>
     </section>
   );

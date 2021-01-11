f138044e2f35ba46a19b2681246b529107eba9de
nepomn9shih <nepomn9shih@gmail.com>
2021-01-11 12:06:14 +0300

    Косметические правки

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,7 @@
 const Footer = () => {
     return (
         <footer class="footer">
-            <div class="footer__copy">Непомнящих Александр &copy; 2020</div>
-            <div class="footer__menu"><a href="https://nepomn9shih.github.io/CatNet" target="_blank">CatNet</a></div>
+            <div>Непомнящих Александр &copy; 2021</div>
         </footer>
     )
 }

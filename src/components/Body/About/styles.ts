e187db2ca38be43b6d779479879e015ba9c71ce6
nepomn9shih <nepomn9shih@gmail.com>
2025-11-10 01:00:48 +0300

    Фикс отображения страницы о сайте на мобилках

--- src/components/Body/About/styles.ts
+++ src/components/Body/About/styles.ts
@@ -13,10 +13,18 @@ export const StyledAboutContent = styled.div`
     border-radius: 10px;
     padding: 16px 16px 0;
     display: flex;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
     b {
         color: var(--main-color);
     }
+
+    @media (min-width: 700px) {
+		flex-direction: row;
+	}
 `;
 
 export const StyledImage = styled.img`

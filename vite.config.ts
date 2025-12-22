98d9bc8faa9827a35780291e95bab99935fbc2e5
nepomn9shih <nepomn9shih@gmail.com>
2025-12-23 00:05:04 +0300

    Правит конфиг vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({command}) =>({
+	base: command === 'build' ? '/MyVisitCard/' : '/',
 	plugins: [
 		react()
 	],
@@ -10,6 +11,12 @@ export default defineConfig({
 		open: true
 	},
 	build: {
-		manifest: true
+        outDir: 'build', // Changes the output directory from 'dist' to 'build'
+        emptyOutDir: true, // Clears the directory before each build (recommended)
+		manifest: true,
+        // If an asset file like (images) whose size is less than 4kb(Default size for vite)
+        // is marked as an inline element and convert it into base64 format so that
+        // It loads without sending a request
+        assetsInlineLimit: 0, // 0kb, set as your minimum file size
 	}
-})
+}));
